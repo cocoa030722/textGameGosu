11bef49b4ff624027da043fbec97e2debbd54db6
CNU202202654 <74183812+cocoa030722@users.noreply.github.com>
2025-01-01 10:20:52 +0000

    Automatic commit before Assistant edits

--- commands.py
+++ commands.py
@@ -10,20 +10,28 @@ class StartCommand(Command):
 
 class ExploreCommand(Command):
     def execute(self, *args, **kwargs):
-        print("Exploring the area...")
-
-class FightCommand(Command):
-    def execute(self, *args, **kwargs):
-        probability = 1  # 80% 확률로 이벤트 발생
+        print("탐색중...")
+        probability = 30  # 30% 확률로 이벤트 발생
         random_value = random.randint(1, 100)  # 1~100 사이의 랜덤 숫자
         if random_value <= probability:
-                print("Entering a fight!")
-        else:
             print("출구 발견!")
             if "game" in kwargs:
                 print("현 층수:",kwargs["game"].current_floor)
                 kwargs["game"].current_floor+=1
+        else:
+            print("탐색 실패")
+            
+            
 
+class FightCommand(Command):
+    def execute(self, *args, **kwargs):
+        probability = 50  # 50% 확률로 이벤트 발생
+        random_value = random.randint(1, 100)  # 1~100 사이의 랜덤 숫자
+        if random_value <= probability:
+            print("전투 시작!")
+        else:
+            print("적 발견하지 못함!")
+            
 class QuitCommand(Command):
     def execute(self, *args, **kwargs):
         print("Quitting the game...")
